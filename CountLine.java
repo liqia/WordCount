5ecefe1c22f7db2730a02e854ed3b086bbe78751
Pang <1286533256@qq.com>
2018-03-18 11:34:18 +0800

    扩展功能基本完成

---
+++ CountLine.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class CountLine {
+    private int cntCode=0, cntNode=0, cntSpace=0;
+    private boolean flagNode = false;
+    public int[] reA(String fileName) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            String line=null;
+            while((line = br.readLine()) != null)
+                pattern(line);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("注释行： " + cntNode);
+        System.out.println("空行： " + cntSpace);
+        System.out.println("代码行： " + cntCode);
+        System.out.println("总行： " + (cntNode+cntSpace+cntCode));
+        return new int[]{cntCode, cntSpace, cntNode};
+    }
+
+    private  void pattern(String line) {
+        // TODO Auto-generated method stub
+        String regxNodeBegin = "\\s*/\\*.*";
+        String regxNodeEnd = ".*\\*/\\s*";
+        String regx = "\\s*//.*|\\}//.*}";
+        String regxSpace = "(\\s*)|(\\s*(\\{|\\})\\s*)";
+        int i=line.length();
+        if(line.matches(regxNodeBegin) && line.matches(regxNodeEnd)){
+            ++cntNode;
+            return ;
+        }
+        if(line.matches(regxNodeBegin)){
+            ++cntNode;
+            flagNode = true;
+        } else if(line.matches(regxNodeEnd)){
+            ++cntNode;
+            flagNode = false;
+        } else if(line.matches(regxSpace)||line.equals("\uFEFF"))
+            ++cntSpace;
+        else if(line.matches(regx))
+            ++cntNode;
+        else if(flagNode)
+            ++cntNode;
+        else ++cntCode;
+    }
+}
--- WordCount.java
+++ WordCount.java
@@ -1,23 +1,13 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordCount {
     public static void main(String[] args) {
-
-        String text=null;
-        try {
-            text = retext(WordCount.class.getResourceAsStream("File.txt"));
-        } catch (NullPointerException e) {
-        }
-        if (text == null) {
-            System.out.println("没有找到文件，请重新输入文件路径");
-        }
-
-        int[] canshu=new int[5];//0-3分别表示字符、单词、行数、代码行数/空行数/注释行、递归处理 的参数存在不存在
+        int[] canshu=new int[5];//0-4分别表示字符、单词、行数、代码行数/空行数/注释行、递归处理 的参数存在不存在
         String file = new String();
         String outputFile = new String();
         String stopListFile = new String();
@@ -31,7 +21,7 @@ public class WordCount {
             else if (args[i].equals("-o"))
             {
                 if (i==args.length-1) erro("参数不匹配");
-                if (Pattern.compile("([a-z]|[A-Z]|[0-9])+.txt").matcher(args[i+1]).find())
+                if (Pattern.compile("([a-z]|[A-Z]|[0-9])+\\.txt").matcher(args[i+1]).find())
                 {
                     outputFile=args[i+1];
                     i++;
@@ -43,7 +33,7 @@ public class WordCount {
             else if (args[i].equals("-e"))
             {
                 if (i==args.length-1) erro("参数不匹配");
-                if (Pattern.compile("([a-z]|[A-Z]|[0-9])+.txt").matcher(args[i+1]).find())
+                if (Pattern.compile("([a-z]|[A-Z]|[0-9])+\\.txt").matcher(args[i+1]).find())
                 {
                     stopListFile=args[i+1];
                     i++;
@@ -52,20 +42,67 @@ public class WordCount {
                     erro("参数不匹配");
                 }
             }
-            else if (Pattern.compile("([a-z]|[A-Z]|[0-9])+.txt").matcher(args[i]).find()) {
+            else if (Pattern.compile("([a-z]|[A-Z]|[0-9])+\\.txt").matcher(args[i]).find()) {
+                if (i==0) erro("输入参数不正确");
                 flag=1;
                 file=args[i];
+            }
+            else {
+                erro("参数不匹配");
             }
         }
         if (flag == 0) erro("参数不匹配");
 
-        System.out.println("world:" + reWorld(text) + "\ncount:" + reCount(text) + "\n" + reLine(text));
+        execute(canshu,file,stopListFile,outputFile);
+    }
+    //根据参数进行计算
+    //0-4分别表示字符、单词、行数、代码行数/空行数/注释行、递归处理 的参数存在不存在
+    static void execute(int[] canshu, String file, String stopListFile, String outputFile) {
+        String outputString=null;
+
+        if (file.matches("\\*\\..+")){
+            String houzui = file.substring(file.lastIndexOf(".")+1, file.length());
+            String houzuiR="\\."+houzui;
+            String[] files=new File(".").list();
+            for (String i : files) {
+                if (i.matches(houzuiR)) {
+                    executOne(canshu,i,stopListFile,outputFile);
+                }
+            }
+        }else {
+            executOne(canshu,file,stopListFile,outputFile);
+        }
+    }
+
+    //执行单个file
+    static void executOne(int[] canshu, String file, String stopListFIle, String outputFile) {
+        StringBuffer outputString=new StringBuffer();
+        String text = retext(file);
+        if (canshu[0] == 1) {
+            outputString.append(file + ",字符数：" + reCount(text)+"\n");
+        }
+        if (canshu[1] == 1) {
+            outputString.append(file + ",单词数：" + reWorld(text,stopListFIle)+"\n");
+        }
+        if (canshu[2] == 1) {
+            outputString.append(file + ",行数：" + reLine(text)+"\n");
+        }
+        if (canshu[3] == 1) {
+            outputString.append(file + ",代码行数/空行数/注释行：" + reA(file)+"\n");
+        }
+        System.out.println(outputString);
+        output(outputString.toString(),outputFile);
     }
 
     //将文件转化为String
-    static String retext(InputStream is) {
+    static String retext(String fileName) {
+        InputStream is= null;
+        try {
+            is = new FileInputStream(fileName);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         String text = null;
-
         try {
             byte[] b = new byte[is.available()];
             is.read(b);
@@ -76,7 +113,6 @@ public class WordCount {
         } catch (IOException var6) {
             var6.printStackTrace();
         }
-
         return text;
     }
 
@@ -86,7 +122,8 @@ public class WordCount {
     }
 
     //统计String单词数
-    static int reWorld(String text) {
+    static int reWorld(String text,String stopListFile) {
+        String[] stopList=retext(stopListFile).split(" ");
         String[] strings = text.split(" |,|\n");
         int stringNull = 0;
         String[] var3 = strings;
@@ -97,9 +134,12 @@ public class WordCount {
             if (i.length() == 0) {
                 ++stringNull;
             }
+            if (stopList.length==0)continue;
+            if (checkIN(i, stopList)) {
+                stringNull++;
+            }
         }
-
-        return strings.length - stringNull;
+        return strings.length - stringNull>=0?strings.length - stringNull:0;
     }
 
     //统计行数
@@ -108,9 +148,35 @@ public class WordCount {
         return strings.length + 1;
     }
 
+    //返回代码行/空行/注释行数
+    static int[] reA(String fileName) {
+        return new CountLine().reA(fileName);
+    }
+
+    //输出结果到指定文件
+    static void output(String text, String outputFile) {
+        try {
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            fos.write(text.getBytes());
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //检查单词是否在停用词表
+    static boolean checkIN(String world, String[] stopList) {
+        for (String stopword : stopList) {
+            if (stopword.equals(world)) return true;
+        }
+        return false;
+    }
+
     //erro
     static void erro(String e) {
         System.out.println(e);
         System.exit(0);
     }
+
+
 }
